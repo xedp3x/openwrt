0aa1b4c68070df67773232a4a04a3e75845ab894
Isaac True <isaac@is.having.coffee>
2015-03-24 22:08:39 +1100

    Added config

--- startvm.sh
+++ startvm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-qemu-system-mips -kernel bin/malta/openwrt-malta-be-vmlinux-initramfs.elf -nographic -m 256 -net nic -net tap,ifname=tap0,script=no,downscript=no
+let "MAC1 = $RANDOM % 99 + 1"
+let "MAC2 = $RANDOM % 99 + 1"
+let "MAC3 = $RANDOM % 99 + 1"
+let "MAC4 = $RANDOM % 99 + 1"
+
+DEV=tap$1
+
+sudo openvpn --mktun --dev $DEV
+sudo brctl addif br0 $DEV
+sudo ip link set dev $DEV up
+qemu-system-mips -kernel bin/malta/openwrt-malta-be-vmlinux-initramfs.elf -nographic -m 256 -net nic,macaddr=52:54:${MAC1}:${MAC2}:${MAC3}:${MAC4} -net tap,ifname=$DEV,script=no,downscript=no
 
