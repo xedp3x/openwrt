68a4ea9c74668af08fd9f577efc1df2d3e906eb0
Isaac True <isaac@is.having.coffee>
2015-03-24 21:42:03 +1100

    Fixed start script

--- startvm.sh
+++ startvm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-qemu-system-mips -kernel bin/malta/openwrt-malta-be-vmlinux-initramfs.elf -nographic -m 256 -net tap,ifname=tap0,script=no,downscript=no
+qemu-system-mips -kernel bin/malta/openwrt-malta-be-vmlinux-initramfs.elf -nographic -m 256 -net nic -net tap,ifname=tap0,script=no,downscript=no
 
